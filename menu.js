ce12f7e31880cc20eb686dcbc5ffcbeecd73503f
KaykeTorres <kaykeallantorresdutra@gmail.com>
2026-06-15 00:23:43 -0300

    add cards

--- menu.js
+++ menu.js
@@ -1,6 +1,18 @@
 const btnHamburguer = document.getElementById('btn-hamburguer');
-const menuMobile = document.getElementById('menu-mobile');
+const btnX = document.getElementById('btn-x');
+// Selecionamos o elemento 'nav' de dentro do header
+const menuMobile = document.querySelector('header nav');
 
+// Função para abrir o menu
 btnHamburguer.addEventListener('click', function() {
-  menuMobile.classList.toggle('ativo');
+    menuMobile.classList.add('ativo');      // Mostra a lista do menu
+    btnHamburguer.classList.add('ativo');  // Esconde o hambúrguer
+    btnX.classList.add('ativo');           // Mostra o X
+});
+
+// Função para fechar o menu (clicando no X)
+btnX.addEventListener('click', function() {
+    menuMobile.classList.remove('ativo');   // Esconde a lista do menu
+    btnHamburguer.classList.remove('ativo');// Mostra o hambúrguer de volta
+    btnX.classList.remove('ativo');        // Esconde o X
 });
